235359434ac005fbee5df8d0556183d69998e74a
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2022-05-17 21:00:44 +0800

    listen on localhost
    (cherry picked from commit 7fe4f66daa9130a93a532ba72970d3668adc4e00)

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use tokio::stream::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<(), &'static str> {
-
-    let mut listener = TcpListener::bind("[::]:6379").await.map_err(|_| "failed to bind on 6379")?;
+    let mut listener = TcpListener::bind("localhost:6379").await.map_err(|_| "failed to bind on 6379")?;
     while let Some(stream) = listener.incoming().filter_map(|x| x.ok()).next().await {
         println!("user connected: {}", stream.peer_addr().map_err(|_| "cannot get peer address")?);
         tokio::spawn(async {
